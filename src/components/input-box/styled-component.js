e051284b6a8d7bc7fe1aa4c6f20fad173f8a87d5
Shikher <sirshikher@gmail.com>
2019-08-25 19:59:41 +0530

    change boarder pixel size

--- src/components/input-box/styled-component.js
+++ src/components/input-box/styled-component.js
@@ -4,7 +4,7 @@ const StyledInput = styled.input`
   font-size: ${props => props.inputBoxFontSize};
   padding: 10px 20px;
   height: ${props => props.inputBoxHeight};
-  border: 1px solid ${props => props.inputBoxBorderColor};
+  border: 10px solid ${props => props.inputBoxBorderColor};
   border-radius: 5px;
   color: ${props => props.inputBoxFontColor};
 
